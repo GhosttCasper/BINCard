a4a47d0cd5924d065585a6835cb7563ceb526824
Vladimir <v.laptev1@g.nsu.ru>
2023-01-22 02:19:56 +0700

    BindingAdapter to set style

--- app/src/main/java/com/example/bincard/BindingAdapters.kt
+++ app/src/main/java/com/example/bincard/BindingAdapters.kt
@@ -3,8 +3,18 @@ package com.example.bincard
 import android.widget.TextView
 import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
+import com.airbnb.paris.extensions.style
 
 @BindingAdapter(value = ["htmlText"])
 fun TextView.setHtmlText(string: String?) {
     text = HtmlCompat.fromHtml(string ?: "", HtmlCompat.FROM_HTML_MODE_COMPACT)
+}
+
+@BindingAdapter("isSelectedStyle")
+fun setStyle(textView: TextView, isSelected: Boolean) {
+    if (isSelected) {
+        textView.style(R.style.Widget_BINCard_TextView_Detail)
+    } else {
+        textView.style(R.style.Widget_BINCard_TextView_InnerLabel)
+    }
 }
